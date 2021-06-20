0e3143f9b14ca82b25249de0bc5a07688c329ab9
Petr Vana <petvana@centrum.cz>
2021-06-20 03:33:44 +0200

    Update model

--- most/code.js
+++ most/code.js
@@ -2,8 +2,8 @@
 
 var drawFce = function () {
     //draw models when all model resources are ready
-    if (houseModel && houseModel.ready) { 
-        houseModel.draw({
+    if (model_protihlukovka && model_protihlukovka.ready) { 
+        model_protihlukovka.draw({
             // 50.2017103N, 14.8604653E
             navCoords: [14.8604653, 50.2017103, 222.1 + 2.3],
             heightMode: 'fix',
@@ -18,7 +18,63 @@ var drawFce = function () {
             //depthOnly: true          //for hit test implementation
             //onLoaded: (fuction(){})  //callback when model is loded
         });
+    } 
+
+    //draw models when all model resources are ready
+    if (model_stratov && model_stratov.ready) { 
+        model_stratov.draw({
+            // 50.2017103N, 14.8604653E
+            navCoords: [14.8604653, 50.2017103, 222.1 + 2.3],
+            heightMode: 'fix',
+            rotation: [0,0,0],
+            scale: [1, 1, 1],
+            ambientLight: [90,90,90],
+
+            //other possible value are:
+            depthOffset: [-15,0,0]  // -10 //when you need some zbuffer tolerance
+                                    // you will probably only need to change first
+                                    // value in the array
+            //depthOnly: true          //for hit test implementation
+            //onLoaded: (fuction(){})  //callback when model is loded
+        });
+    } 
+
+    //draw models when all model resources are ready
+    if ((!visible) && model_A && model_A.ready) { 
+        model_A.draw({
+            // 50.2017103N, 14.8604653E
+            navCoords: [14.8604653, 50.2017103, 222.1 + 2.3],
+            heightMode: 'fix',
+            rotation: [0,0,0],
+            scale: [1, 1, 1],
+            ambientLight: [90,90,90],
+
+            //other possible value are:
+            depthOffset: [0,0,0]  // -10 //when you need some zbuffer tolerance
+                                    // you will probably only need to change first
+                                    // value in the array
+            //depthOnly: true          //for hit test implementation
+            //onLoaded: (fuction(){})  //callback when model is loded
+        });
     }    
+
+    if ((visible) && model_B && model_B.ready) { 
+        model_B.draw({
+            // 50.2017103N, 14.8604653E
+            navCoords: [14.8604653, 50.2017103, 222.1 + 2.3],
+            heightMode: 'fix',
+            rotation: [0,0,0],
+            scale: [1, 1, 1],
+            ambientLight: [90,90,90],
+
+            //other possible value are:
+            depthOffset: [-0,0,0]  // -10 //when you need some zbuffer tolerance
+                                    // you will probably only need to change first
+                                    // value in the array
+            //depthOnly: true          //for hit test implementation
+            //onLoaded: (fuction(){})  //callback when model is loded
+        });
+    }  
 
     if (visible && carModel && carModel.ready) { 
         carModel.draw({
